672cae60ea22f2202091147b1679a35f86f9f5cd
Wu <wkz808@163.com>
2018-01-24 22:13:46 +0800

    jsw build enabled, stable

--- dhandho-jsclient/src/main/java/app/dhojsw/ng/AppModule.java
+++ dhandho-jsclient/src/main/java/app/dhojsw/ng/AppModule.java
@@ -2,9 +2,10 @@ package app.dhojsw.ng;
 
 import app.dhojsw.ng.my.MyComponent;
 import def.angular.core.NgModule;
+import def.angular.platform_browser.BrowserModule;
 
 @NgModule(//
-		imports = {}, //
+		imports = { BrowserModule.class }, //
 		declarations = { AppComponent.class, MyComponent.class }, //
 		bootstrap = { AppComponent.class }, //
 		providers = {}//
