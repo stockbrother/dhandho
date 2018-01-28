44bd18c1eb0d7773e9066d9b32c57172f8787278
Wu <wkz808@163.com>
2018-01-28 13:59:25 +0800

    testing

---
+++ dhandho-jsclient/src/main/java/app/dhojsw/ng/service/LoggerService.java
@@ -0,0 +1,27 @@
+package app.dhojsw.ng.service;
+
+import static def.dom.Globals.console;
+
+public class LoggerService {
+
+	public void info(Object msg, Object... args) {
+		console.info(msg, args);
+	}
+
+	public void debug(Object msg, Object... args) {
+		console.debug(String.valueOf(msg), args);
+	}
+
+	public void trace(Object msg, Object... args) {
+		console.info(msg, args);
+	}
+
+	public void warn(Object msg, Object... args) {
+		console.warn(msg, args);
+	}
+
+	public void error(Object msg, Object... args) {
+		console.info(msg, args);
+	}
+
+}
