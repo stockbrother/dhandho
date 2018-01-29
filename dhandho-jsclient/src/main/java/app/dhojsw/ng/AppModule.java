069b03efe32a08b7a64081a932fee755319bbe69
wu <wkz808@163.com>
2018-01-29 18:06:36 +0800

    fix ng build, runnable, stable

--- dhandho-jsclient/src/main/java/app/dhojsw/ng/AppModule.java
+++ dhandho-jsclient/src/main/java/app/dhojsw/ng/AppModule.java
@@ -1,13 +1,16 @@
 package app.dhojsw.ng;
 
+import app.dhojsw.ng.command.CommandComponent;
 import app.dhojsw.ng.my.MyComponent;
+import def.angular.common_http.HttpClientModule;
 import def.angular.core.NgModule;
+import def.angular.forms.FormsModule;
 import def.angular.platform_browser.BrowserModule;
 
 @NgModule(//
-		imports = { BrowserModule.class }, //
-		declarations = { AppComponent.class, MyComponent.class }, //
-		bootstrap = { AppComponent.class }, //
+		imports = { BrowserModule.class, FormsModule.class, HttpClientModule.class }, //
+		declarations = { AppComponent.class, MyComponent.class, CommandComponent.class }, //
+		bootstrap = { AppComponent.class, CommandComponent.class }, //
 		providers = {}//
 )
 
