89cf21f24bb57ea74b9ae6344384fe48e504c419
wu <wkz808@163.com>
2018-02-01 18:09:21 +0800

    trying to implement RouterModule

---
+++ dhandho-jsclient/src/main/java/app/dhojsw/ng/util/RouterModuleForRoot.java
@@ -0,0 +1,19 @@
+package app.dhojsw.ng.util;
+
+import def.angular.core.NgModule;
+import def.angular.router.RouterModule;
+
+/**
+ * 
+ * @author Wu
+ *
+ */
+@NgModule(//
+		imports = { RouterModule.class }, //
+		declarations = {  }, //
+		bootstrap = {  }, //
+		providers = {RouterModule.class}//
+)
+public class RouterModuleForRoot {
+
+}
---
+++ dhandho-jsclient/src/main/java/def/angular/router/RouterModule.java
@@ -0,0 +1,9 @@
+package def.angular.router;
+
+import def.angular.core.ModuleWithProviders;
+
+public class RouterModule {
+	
+	public static native ModuleWithProviders forRoot(Routes routes);
+	
+}
