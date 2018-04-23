ba9fc94d7e897b77355d4d3bb153603cc7702632
wu <wkz808@163.com>
2018-04-23 17:38:09 +0800

    a

--- dhandho-core/src/main/java/cc/dhandho/commons/http/JsonElementHttpResponseHandler.java
+++ dhandho-core/src/main/java/cc/dhandho/commons/http/JsonElementHttpResponseHandler.java
@@ -12,13 +12,13 @@ import com.age5k.jcps.framework.handler.Handler3;
 import com.google.gson.JsonElement;
 
 import cc.dhandho.util.JsonUtil;
-import junit.framework.TestCase;
+//import junit.framework.TestCase;
 
 public class JsonElementHttpResponseHandler implements Handler3<CloseableHttpResponse, JsonElement> {
 	@Override
 	public JsonElement handle(CloseableHttpResponse arg0) {
 		StatusLine sl = arg0.getStatusLine();
-		TestCase.assertEquals(sl.toString(), 200, sl.getStatusCode());
+	//	TestCase.assertEquals(sl.toString(), 200, sl.getStatusCode());
 		try {
 			InputStream is = arg0.getEntity().getContent();
 			JsonElement json = JsonUtil.parse(is, Charset.forName("utf8"));
